3840e3d257428c0f52287086253d7a48276ebdea
Tom Morgan <tommorgan.digitaldwp@gmail.com>
2018-11-16 12:49:09 +0000

    reenabling the servie workers

--- scripts/app.js
+++ scripts/app.js
@@ -15,11 +15,11 @@
     localStorage.setItem("message", message);
   });
 
-  // if ('serviceWorker' in navigator) {
-  //   navigator.serviceWorker
-  //            .register('./service-worker.js')
-  //            .then(function() { console.log('Service Worker Registered'); });
-  // }
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+             .register('./service-worker.js')
+             .then(function() { console.log('Service Worker Registered'); });
+  }
 
   // uncomment to reset the dates array.
   // var dates = [];
